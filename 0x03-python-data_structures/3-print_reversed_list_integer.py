d0439fd1037a90c34b989edac8ad291bfec04d08
Ishaq <umarishaq26@gmail.com>
2022-12-03 00:04:27 -0800

    My list in reverse order

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -5,7 +5,7 @@
 def print_reversed_list_integer(my_list=[]):
     n = len(my_list) - 1
     if n < 0:
-        print(my_list)
+        print(my_list[0:])
     while(n >= 0):
         print("{:d}".format(my_list[n]))
         n -= 1
