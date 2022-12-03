a4a5f7d091f5d998f41922f15ea62912b0988193
Ishaq <umarishaq26@gmail.com>
2022-12-03 14:47:21 -0800

    No replace my list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-"""Printing list in resverse order"""
-
-
 def print_reversed_list_integer(my_list=[]):
     for i in range(1, len(my_list) + 1):
         print("{:d}".format(my_list[-i]))
