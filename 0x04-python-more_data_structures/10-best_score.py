59410c09b02f5bf96881ec1b5d73ca5e2a49f980
Ishaq <umarishaq26@gmail.com>
2022-12-07 13:21:28 -0800

    Error fixed

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     """Return best score"""
-    if not isinstance(a_dictionary, dict) or a_dictionary is None:
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return
     best_key = [x for x in a_dictionary.keys()][0]
     for key in a_dictionary:
