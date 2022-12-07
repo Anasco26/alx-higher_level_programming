32984a15b9c916b492931fb33b04d4eb90ac39bc
Ishaq <umarishaq26@gmail.com>
2022-12-07 13:09:29 -0800

    Return best key

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     """Return best score"""
-    if a_dictionary == None:
+    if a_dictionary is None:
         return
     best_key = [x for x in a_dictionary.keys()][0]
     for key in a_dictionary:
         if a_dictionary[best_key] < a_dictionary[key]:
             best_key = key
     return best_key
-            
-    
